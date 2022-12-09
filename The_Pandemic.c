873de1dc14ab674cd48207f957866bb519421815
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-09 10:48:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n==1)     {         printf("%d",0);     }     else if(n==2)     {          printf("%d",1);     }     else     {          printf("%d",2);     } }

---
+++ The_Pandemic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("%d",0);
+    }
+    else if(n==2)
+    {
+         printf("%d",1);
+    }
+    else
+    {
+         printf("%d",2);
+    }
+}
