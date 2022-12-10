ca2cd42f8364f7ae84a2f9f71b03607a7dee3546
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-10 21:00:24 +0530

    #include<stdio.h> int main() {     double s;     double a,v;     scanf("%lf",&s);     a=6*s*s;     v=s*s*s;     printf("Surface area = %0.2lf and Volume = %0.2lf",a,v); }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    double s;
+    double a,v;
+    scanf("%lf",&s);
+    a=6*s*s;
+    v=s*s*s;
+    printf("Surface area = %0.2lf and Volume = %0.2lf",a,v);
+}
+
