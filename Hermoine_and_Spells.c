10aebd9c61773bf497342fde303de8da418da88c
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-11 06:29:32 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(x<y&&x<z)     {         printf("%d",y+z);     }     else if(y<x&&y<z)     {         printf("%d",x+z);     }      else     {         printf("%d",y+x);     } }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,43 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,x,y;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b&&a>c)
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(x<y&&x<z)
     {
-        x=a;
-        if(b>c)
-        {
-           y=b; 
-        }
-        else
-        {
-            y=c;
-        }
+        printf("%d",y+z);
     }
-    else if(b>a&&b>c)
+    else if(y<x&&y<z)
     {
-        x=b;
-        if(a>c)
-        {
-            y=a;
-        }
-        else
-        {
-            y=c;
-        }
+        printf("%d",x+z);
     }
-    else
+     else
     {
-        x=c;
-        if(a>b)
-        {
-            y=a;
-        }
-        else
-        {
-            y=b;
-        }
+        printf("%d",y+x);
     }
-    printf("%d",x+y);
 }
