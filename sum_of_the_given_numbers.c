4b1a640d04c0a5cda8644f92fc1b870d56dec4fd
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-14 19:51:40 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int x,y,z;         scanf("%d%d",&x,&y);         z=x+y;         printf("%d ",z);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y,z;
+        scanf("%d%d",&x,&y);
+        z=x+y;
+        printf("%d
+",z);
+    }
+}
