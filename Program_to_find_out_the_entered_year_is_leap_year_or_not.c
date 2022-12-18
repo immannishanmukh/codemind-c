04c10fb15e060e8207649c6773357c79a7186408
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-18 20:04:16 +0530

    #include<stdio.h>
    int main()
    {
        int  n;
        scanf("%d",&n);
        if(n%400==0)
        {
            printf("True");
            
        }
        else if(n%100!=0)
        {
           if(n%4==0)
           {
               printf("True");
           }
           else
           {
               printf("False");
           }
           
        }
        else
        {
            printf("False");
        }
    }

---
+++ Program_to_find_out_the_entered_year_is_leap_year_or_not.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int  n;
+    scanf("%d",&n);
+    if(n%400==0)
+    {
+        printf("True");
+        
+    }
+    else if(n%100!=0)
+    {
+       if(n%4==0)
+       {
+           printf("True");
+       }
+       else
+       {
+           printf("False");
+       }
+       
+    }
+    else
+    {
+        printf("False");
+    }
+}
