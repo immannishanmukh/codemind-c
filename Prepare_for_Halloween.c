b543bf510e2e95caf4bbe388d17b283cb963d8e3
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-29 02:24:54 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {     float a,b,m,n;     scanf("%f%f",&a,&b);     m=a*2;     n=b*5;         if(m==n)         {             printf("Either ");         }     else if(m<n)     {        printf("Candy ");     }     else     {         printf("Chocolate ");     }     } }

---
+++ Prepare_for_Halloween.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+    float a,b,m,n;
+    scanf("%f%f",&a,&b);
+    m=a*2;
+    n=b*5;
+        if(m==n)
+        {
+            printf("Either
+");
+        }
+    else if(m<n)
+    {
+       printf("Candy
+");
+    }
+    else
+    {
+        printf("Chocolate
+");
+    }
+    }
+}
