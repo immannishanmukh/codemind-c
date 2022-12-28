4183c09b03f48653b39b948debee9bce558d5443
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-28 22:03:00 +0530

    #include<stdio.h> int main() {     int n,v;     scanf("%d",&n);     if(n!=0)     {         printf("%d ",n);     }     while(n!=1)     {         if(n%2==0)         {             n=n/2;             printf("%d ",n);         }         else         {             n=3*n+1;             printf("%d ",n);         }       //  printf("%d ",n);     }
    }

---
+++ Collatz_Sequence_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,v;
+    scanf("%d",&n);
+    if(n!=0)
+    {
+        printf("%d ",n);
+    }
+    while(n!=1)
+    {
+        if(n%2==0)
+        {
+            n=n/2;
+            printf("%d ",n);
+        }
+        else
+        {
+            n=3*n+1;
+            printf("%d ",n);
+        }
+      //  printf("%d ",n);
+    }
+    
+}
