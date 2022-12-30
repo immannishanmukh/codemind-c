3464e5ba9f92d37932d6f8113cd1ffe99fc8cc64
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-30 23:10:55 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int n;         scanf("%d",&n);         if(n%2==0)         {             printf("%d ",n/2);         }         else         {             printf("%d ",(n+1)/2);         }     } }

---
+++ Playing_a_game_with_Hoops.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n%2==0)
+        {
+            printf("%d
+",n/2);
+        }
+        else
+        {
+            printf("%d
+",(n+1)/2);
+        }
+    }
+}
