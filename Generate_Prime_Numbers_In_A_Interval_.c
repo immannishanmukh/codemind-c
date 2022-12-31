e1d4eecff7bbb1b958950f6ed6f33f132e17ea7c
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-31 10:21:14 +0530

    #include<stdio.h> int ppr(int x) {     int i,c=0;     for(i=1;i<=x;i++)     {         if(x%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int l,u,j;     scanf("%d%d",&l,&u);     for(j=l;j<=u;j++)     {         if(ppr(j))         {         printf("%d ",j);         }     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int ppr(int x)
+{
+    int i,c=0;
+    for(i=1;i<=x;i++)
+    {
+        if(x%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int l,u,j;
+    scanf("%d%d",&l,&u);
+    for(j=l;j<=u;j++)
+    {
+        if(ppr(j))
+        {
+        printf("%d
+",j);
+        }
+    }
+}
