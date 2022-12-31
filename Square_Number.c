cef904a7baf1deef03fc63d77b2c8375a27ec85e
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2022-12-31 22:29:43 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,s=0,i,j,f=0;     scanf("%d",&n);     for(i=0;i<=n;i++)     {         for(j=0;j<=n;j++)         {             if(i!=j)             {             s=i*i+j*j;             if(n==s)             {                 f=1;                 break;             }             }         }     }     if(f==0)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Square_Number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,s=0,i,j,f=0;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+        {
+            if(i!=j)
+            {
+            s=i*i+j*j;
+            if(n==s)
+            {
+                f=1;
+                break;
+            }
+            }
+        }
+    }
+    if(f==0)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
