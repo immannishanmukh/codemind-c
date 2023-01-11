0527aa8c7589796f0cc84e289d4e4a62ae02f1a6
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2023-01-12 00:06:31 +0530

    #include<stdio.h> int main() {     int n,i,o=0,e=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {            e++;         }         else         {             o++;         }     }     printf("%d %d",e,o);     return 0; }

---
+++ Count.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,o=0,e=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+           e++; 
+        }
+        else
+        {
+            o++;
+        }
+    }
+    printf("%d %d",e,o);
+    return 0;
+}
