3d95846ff4dc53d160792837cabb4df1758e55c6
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2023-01-22 21:58:29 +0530

    #include <stdio.h> int main ( ) {     int n , m ;     scanf ( "%d %d" , &n , &m ) ;     int a [ n ] [ m ] ;     for ( int i = 0 ; i < n ; i++ )     {         for ( int j = 0 ; j < m ; j++ )         {             scanf ( "%d" , &a [ i ] [ j ] ) ;         }     }     for ( int i = 0 ; i < m ; i++ )     {         int s = 0 ;         for ( int j = 0 ; j < n ; j++ )         {             if ( a [ j ] [ i ] > s )             {                 s = a [ j ] [ i ] ;             }         }         printf ( "%d " , s ) ;     }
    }

---
+++ Maximum_element_in_each_column.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main ( ) 
+{
+    int n , m ;
+    scanf ( "%d %d" , &n , &m ) ;
+    int a [ n ] [ m ] ;
+    for ( int i = 0 ; i < n ; i++ )
+    {
+        for ( int j = 0 ; j < m ; j++ )
+        {
+            scanf ( "%d" , &a [ i ] [ j ] ) ;
+        }
+    }
+    for ( int i = 0 ; i < m ; i++ )
+    {
+        int s = 0 ;
+        for ( int j = 0 ; j < n ; j++ )
+        {
+            if ( a [ j ] [ i ] > s )
+            {
+                s = a [ j ] [ i ] ;
+            }
+        }
+        printf ( "%d
+" , s ) ;
+    }
+    
+    
+}
